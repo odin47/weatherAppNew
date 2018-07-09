8a3fac097410a931dd72b2ffe430a8e2670766c5
odin47 <pddeepak599@gmail.com>
2018-07-09 18:13:05 +0530

    changed server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/weatherAppNew/dist'));
+app.use(express.static(__dirname + '/weather-app-new/dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'./weatherAppNew/dist/index.html'));
+res.sendFile(path.join(__dirname+'./weather-app-new/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
